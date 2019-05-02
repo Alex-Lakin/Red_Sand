1ed1d9d2c44738d3d74c6b24fdb992ee45cdb304
AlexLakin <alex.s.lakin@gmail.com>
2019-05-02 19:27:23 +0100

    Implemented item PickUp system
    Player can now pick up items and weapons etc off the ground. Player no longer spawns with sword, and must pick it up.

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    [SerializeField] private GameObject prefabToInstantiate;
+    [SerializeField] private Transform transformToParentInstanceTo;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void pickUpAction()
+    {
+        Instantiate(prefabToInstantiate, transformToParentInstanceTo);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PickUpDetector.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpDetector : MonoBehaviour {
+
+    List<PickUp> allPickUps;
+    [SerializeField] private float maxPickUpDist;
+
+    // Use this for initialization
+    void Start()
+    {
+        allPickUps = new List<PickUp>();
+
+        findAndAddAllPickUps(allPickUps);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get Imput
+        bool p = Input.GetButtonDown("PickUp");
+
+        if (p) {
+            PickUp closestPickUp = findClosestPickUp(allPickUps);
+
+            if (closestPickUp != null)
+            {
+                float distanceToPickUp = findDistanceToClosestPickUp(closestPickUp);
+
+                if (distanceToPickUp < maxPickUpDist)
+                {
+                    usePickUp(closestPickUp);
+                }
+            }
+        }
+    }
+
+    void findAndAddAllPickUps(List<PickUp> listOfPickUps)
+    {
+        listOfPickUps.AddRange(GameObject.FindObjectsOfType<PickUp>());
+    }
+
+    void removePickUpFromList(PickUp pickUpToRemove, List<PickUp> listOfPickUps)
+    {
+        for (int i = 0; i < listOfPickUps.Count; i++)
+        {
+            if (listOfPickUps[i] == pickUpToRemove)
+            {
+                listOfPickUps.RemoveAt(i);
+            }
+        }
+    }
+
+    PickUp findClosestPickUp(List<PickUp> listOfPickUps)
+    {
+        float distanceToClosestPickUp = Mathf.Infinity;
+        PickUp closestPickUp = null;
+
+        foreach (PickUp currentPickUp in listOfPickUps)
+        {
+            //Find closest PickUp
+            float distanceToCurrentPickUp = Vector3.Distance(currentPickUp.transform.position, this.transform.position);
+            if (distanceToCurrentPickUp < distanceToClosestPickUp)
+            {
+                distanceToClosestPickUp = distanceToCurrentPickUp;
+                closestPickUp = currentPickUp;
+            }
+        }
+        return closestPickUp;
+    }
+
+    float findDistanceToClosestPickUp(PickUp closestPickUp)
+    {
+        float dist = Vector3.Distance(closestPickUp.transform.position, this.transform.position);
+        return dist;
+    }
+
+    void usePickUp(PickUp closestPickUp)
+    {
+        closestPickUp.pickUpAction();
+        removePickUpFromList(closestPickUp, allPickUps);
+    }
+}
